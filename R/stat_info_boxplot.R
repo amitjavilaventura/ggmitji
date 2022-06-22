7275b856a588f768e6364c8c7fcef74d00acb253
amitjavilaventura <adria.mitjavila94@gmail.com>
2022-06-22 23:05:54 +0200

    update stat_info_boxplot()

--- R/stat_info_boxplot.R
+++ R/stat_info_boxplot.R
@@ -10,26 +10,26 @@
 #'
 #' @seealso `ggplot2::stat_summary()`
 #'
-#' @param statistic Character of length 1. One of c("n", "mean", "median", "sd"). Default = "n".
+#' @param statistic Character of length 1. One of c("n", "mean", "median", "sd", 'sum'). Default = "n".
 #' @param label Character of length 1. The label to show with the statistic, e.g. "N=". Default = "".
-#' @param y Character or numeric of length 1. If character, one of c("upper", "lower", "min", "max", "q1", "q2", "median", "q3"). If numeric, the Y coordinates of the label.
+#' @param y Character or numeric of length 1. If character, one of c("upper", "lower", "min", "max", "q1", "q2", "median", "q3"). If numeric, the Y coordinates of the label. Default = 0.
 #' @param width Numeric of length 1. Passed through poistion_dodge(width=width).
 #' @param text_color Character of length 1. Color of the label.
 #' @param text_size Numeric of length 1. Size of the label.
 #'
 #' @export
 
-stat_info_boxplot <- function(text_size = 2,
+stat_info_boxplot <- function(statistic = "n",
+                              label = "",
+                              y = 0,
+                              text_size = 2,
                               text_color = "black",
-                              width = .5,
-                              y = "lower",
-                              statistic = "n",
-                              label = ""){
+                              width = .5){
 
   # Check and format the inputs.
   if(is.character(y)){ y <- tolower(y) }
   if(is.character(statistic)){ statistic <- tolower(statistic) }
-  else { stop("'statistic' must be one of 'n', 'mean', 'median', 'sd' ") }
+  else { stop("'statistic' must be one of 'n', 'mean', 'median', 'sd', 'sum' ") }
 
   # Define internal function to calculate the Y, and define the label.
   calc_info_boxplot <- function(x){
@@ -52,6 +52,7 @@ stat_info_boxplot <- function(text_size = 2,
     else if(statistic == "mean"){ label = paste0(label, round(mean(x), digits = 2), sep = "")}
     else if(statistic == "median"){ label = paste0(label, round(median(x), digits = 2), sep = "")}
     else if(statistic == "sd"){ label = paste0(label, round(sd(x), digits = 2), sep = "")}
+    else if(statistic == "sum"){ label = paste0(label, sum(x), sep = "")}
 
     # Create the dataframe with the values
     n <- data.frame(y = y, label = label)
